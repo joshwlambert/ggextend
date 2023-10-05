819995c4643f8d27c203e53a414347d98549f6e9
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-05 15:37:46 +0100

    updated sim_data tests

--- tests/testthat/test-sim_data.R
+++ tests/testthat/test-sim_data.R
@@ -1,13 +1,13 @@
 test_that("sim_data works as expected", {
   dat <- sim_data(t = 10, b = 1, d = 1)
   expect_s3_class(dat, class = "data.frame")
-  expect_identical(dim(dat), c(11L, 2L))
-  expect_named(dat, c("time", "system"))
+  expect_identical(dim(dat), c(11L, 3L))
+  expect_named(dat, c("time", "system", "rate"))
 })
 
 test_that("sim_data 2 rate works as expected", {
   dat <- sim_data(t = 10, b = 1, d = 1, t2 = 5, b2 = 5, d2 = 2)
   expect_s3_class(dat, class = "data.frame")
-  expect_identical(dim(dat), c(11L, 2L))
-  expect_named(dat, c("time", "system"))
+  expect_identical(dim(dat), c(11L, 3L))
+  expect_named(dat, c("time", "system", "rate"))
 })
