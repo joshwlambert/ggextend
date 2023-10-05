323ce0688e072dd6421cafd78022cd8a388a9dc4
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-05 11:06:22 +0100

    initial commit

---
+++ R/tmpfunc.R
@@ -0,0 +1,9 @@
+#' Temporary function
+#'
+#' @param x Argument
+#'
+#' @return Called for side-effects
+#' @export
+foo <- function(x) {
+  print(x)
+}
---
+++ tests/testthat/test-tmpfunc.R
@@ -0,0 +1,3 @@
+test_that("tmpfunc works", {
+  expect_output(foo(1))
+})
